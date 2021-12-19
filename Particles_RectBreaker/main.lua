b2da5fe33b35a84773c233469237f2c0dc6d425a
Serebrennikov-Dmitry <argentum0205@gmail.com>
2021-12-19 11:50:07 +0800

    project finish

--- Particles_RectBreaker/main.lua
+++ Particles_RectBreaker/main.lua
@@ -2,46 +2,52 @@ require("vector")
 require("particle")
 
 function love.load()
-   width = love.graphics.getWidth()
+   length = love.graphics.getWidth()
    height = love.graphics.getHeight()
-   --particle = Particle:create(Vector:create(width / 2, height / 2))
+   --particle = Particle:create(Vector:create(length / 2, height / 2))
    
-   particles = {}
-   for i = 1, 17 do 
-      local location = Vector:create(math.random(1, width), math.random(1, height))
-      particle = Particle:create(location)
-      table.insert(particles, particle)
+
+   systems = {}
+
+   for i = 1, 3 do
+      table.insert(systems, system)
    end
-   
-   --system = ParticleSystem:create(Vector:create(width / 2, height / 2), 100)
-   --gravity = Vector:create(0, 0.1)
+
+   systems[1] =  ParticleSystem:create(Vector:create(150, 200), 50, {1, 0,  0})
+   systems[2] = ParticleSystem:create(Vector:create(300, 250), 100, {0, 1, 0})
+   systems[3] = ParticleSystem:create(Vector:create(500, 300), 200, {0, 0, 1})
+
+   --system = ParticleSystem:create(Vector:create(length / 2, height / 2), 100)
+   gravity = Vector:create(0, 0.1)
 end
 
 function love.update(dt)
     --particle:update()
     --system:applyForce(gravity)
     --system:update()
-
-   for index, particle in ipairs(particles) do
-      particle:update()  
+   for k,v in pairs(systems) do
+      systems[k]:update()
+      systems[k]:applyForce(gravity)  
    end
-
 end
 
 function love.draw()
    --particle:draw()
    --system:draw()
-
-   for index, particle in ipairs(particles) do
-      particle:draw()  
+   for k,v in pairs(systems) do 
+      systems[k]:draw()  
    end
 end
 
 function love.mousepressed(x, y, button, istouch, presses)
-   if button == 1 then
-      for index, particle in ipairs(particles) do
-         local x, y = love.mouse.getPosition()
-         --if (x)  
+   if button == 1 then 
+      for k,v in pairs(systems) do
+         if x >= systems[k].origin.x and x <= systems[k].origin.x + systems[k].length 
+            and y >= systems[k].origin.y and y <= systems[k].origin.y + systems[k].length then
+            for k, v in pairs(systems[k].particles) do
+               v.isActive = true			
+            end
+         end
       end
-   end
+  end
 end
--- Particles_RectBreaker/particle.lua
+++ Particles_RectBreaker/particle.lua
@@ -1,28 +1,33 @@
 Particle = {}
 Particle.__index = Particle
 
-function Particle:create(location)
+function Particle:create(location, color)
     local particle = {}
     setmetatable(particle, Particle)
     particle.location = location
-    --particle.acceleration = Vector:create(0, 0.05)
-    --particle.velocity = Vector:create(math.random(-400, 400) / 100, math.random(-200, 0) / 100)
-    --particle.lifespan = 50
-    --particle.decay = math.random(3, 8) / 10
-    
+    particle.acceleration = Vector:create(0, 0.05)
+    particle.velocity = Vector:create(math.random(-400, 400) / 100, math.random(-200, 0) / 100)
+    particle.lifespan = 50
+    particle.decay = math.random(3, 8) / 10
+    particle.isActive = false
+    particle.color = color
     --particle.texture = love.graphics.newImage("resources/coin.png")
     return particle 
 end
 
 function Particle:update()
-    --self.velocity:add(self.acceleration)
-    --self.location:add(self.velocity)
-    --self.acceleration:mul(0)
-    --self.lifespan = self.lifespan - self.decay
+    if self.isActive == true then
+        self.velocity:add(self.acceleration)
+        self.location:add(self.velocity)
+        self.acceleration:mul(0)
+        self.lifespan = self.lifespan - self.decay
+    end
 end
 
 function Particle:applyForce(force)
-    self.acceleration:add(force)
+    if self.isActive == true then
+        self.acceleration:add(force)
+    end
 end
 
 function Particle:isDead()
@@ -32,8 +37,8 @@ end
 function Particle:draw()
     r, g, b, a = love.graphics.getColor()
     --love.graphics.setColor(1, 1, 1, self.lifespan / 100)
-    love.graphics.rectangle("line", self.location.x, self.location.y, 30, 30)
-    --love.graphics.line(self.location.x1, self.location.y1, self.location.x2, self.location.y2)
+    love.graphics.setColor(self.color[1],  self.color[2], self.color[3], self.lifespan / 100) --
+    love.graphics.rectangle("line", self.location.x, self.location.y, 10, 10)
     --love.graphics.draw(self.texture, self.location.x, self.location.y, 0.1, 0.1)
     love.graphics.setColor(r, g, b, a)
 end
@@ -42,14 +47,23 @@ end
 ParticleSystem = {}
 ParticleSystem.__index = ParticleSystem
 
-function ParticleSystem:create(origin, n, cls)
+function ParticleSystem:create(origin, length, color, cls)
     local system = {}
     setmetatable(system, ParticleSystem)
     system.origin = origin
-    system.n = n or 10
     system.cls = cls or Particle
     system.particles = {}
     system.index = 1
+    system.length = length
+
+    local index = 1
+
+    for i = 1, length / 10 do
+        for j = 1, length / 10 do
+            system.particles[index] = system.cls:create(system.origin:copy() + Vector:create((i - 1) * 10, (j - 1) * 10), color)
+            index = index + 1
+        end
+    end
     return system
 end
 
@@ -61,18 +75,9 @@ function ParticleSystem:draw()
 end
 
 function ParticleSystem:update()
-    if #self.particles < self.n then
-        --table.insert(self.particles, self.cls:create(self.origin:copy()))
-        self.particles[self.index] = self.cls:create(self.origin:copy())
-        self.index = self.index + 1
-    end
     for k, v in pairs(self.particles) do
-        if v:isDead() then
-            v = self.cls:create(self.origin:copy())
-            self.particles[k] = v
-        end
-        v:update()
-    end
+		v:update()
+	end
 end
 
 function ParticleSystem:applyForce(force)
